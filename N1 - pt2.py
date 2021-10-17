110366ce32948d8ab1c6496f560d40bd29e617e0
NAT4N <68129527+NAT4N@users.noreply.github.com>
2021-10-17 19:46:12 -0300

    Add files via upload

---
+++ N1 - pt2.py
@@ -0,0 +1,59 @@
+usuarios = []
+
+getData = {
+  "Nome": "",
+  "E-mail": ""
+}
+
+def menu():
+    print("============================================\n")
+    print("1- Cadastrar novos usuários\n")
+    print("2- Listar usuários (ordem de cadrasto)\n")
+    print("3- Listar usuário (ordem alfabética)\n")
+    print("4- Verificar se o usuário está na lista\n")
+    print("5- Remover usuário\n")
+    print("6- Alterar o nome de um usuário\n")
+    print("0- Fechar o Menu\n")
+    print("============================================\n")
+    choice = int(input(""))
+
+    if(choice == 1):
+        print("\n ========================= \n")
+        print("Cadastrar Usuário\n")
+        print("Digite o nome completo\n")
+        nomeCompleto = input("")
+        print("Digite o E-mail\n")
+        email = input("")
+        cadastrarUsuarios(nomeCompleto, email)
+    elif(choice == 2):
+        print("\n ========================= \n")
+        print("Listar Usuário (ordem de cadastro)\n")
+        listarUsuariosOrdemCadastro()
+    elif(choice == 3):
+        print("\n ========================= \n")
+        print("Listar Usuário (ordem de alfabética)\n")
+        listarUsuariosOrdemAlfabetica()
+    elif(choice == 4):
+        print("\n ========================= \n")
+        print("Verificar Usuário na Lista\n")
+        print("Digite o nome do usuário: \n")
+        nome = input("")
+        verificarLista(nome)
+    elif(choice == 5):
+        print("\n ========================= \n")
+        print("Remover usuário\n")
+        print("Digite o e-mail do usuário: \n")
+        email = input("")
+        removerUsuarios(email)
+    elif(choice == 6):
+        print("\n ========================= \n")
+        print("Alterar o nome de um Usuário\n")
+        print("Digite o e-mail do usuário:\n")
+        email = input("")
+        alterarUsuario(email)
+    elif(choice == 0):
+        print("\n ========================= \n")
+        print("Encerrando...\n")
+    else:
+        print("Escolha uma opção valida")
+        continuarMenu()
